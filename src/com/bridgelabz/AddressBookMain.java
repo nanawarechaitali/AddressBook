cdad72a5aedfc592e7763738e3becd8b521d1c84
Your Name <nanawarechaitali4196@gmail.com>
2023-02-18 08:39:39 +0530

    EdiingContact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,6 +1,10 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+    static Scanner scanner=new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook System on Master Branch");
         AddressBookMethods addressBookMethods = new AddressBookMethods();
@@ -8,6 +12,17 @@ public class AddressBookMain {
         addressBookMethods.newContact();
 
         System.out.println(addressBookMethods.list);
+
+        System.out.println("If you want to edit any contact press 1");
+        int check= scanner.nextInt();
+        if(check==1){
+            addressBookMethods.editContact();
+
+        }else{
+            System.out.println("Thanks for using this address-book");
+        }
+        System.out.println(addressBookMethods.list);
+
 
 
     }
--- src/com/bridgelabz/AddressBookMethods.java
+++ src/com/bridgelabz/AddressBookMethods.java
@@ -33,5 +33,67 @@ public class AddressBookMethods {
         list.add(contact);
 
     }
+    public void editContact(){
+        System.out.println("Enter first name to edit the contact");
+        String name = sc.nextLine();
+
+        for (Contact con : list){
+            if(con.getFirstName().equals(name)){
+
+
+                System.out.println("Which details you want to edit");
+                System.out.println("press 1 for first name");
+                System.out.println("Press 2 for last name");
+                System.out.println("Press 3 for address");
+                System.out.println("Press 4 for city");
+                System.out.println("Press 5 for state");
+                System.out.println("Press 6 for zip");
+                String choice = sc.nextLine();
+
+                switch (choice){
+                    case "1" :
+                        System.out.println("Enter new first name");
+                        con.setFirstName(sc.nextLine());
+                        break;
+
+                    case "2" :
+                        System.out.println("Enter new last name");
+                        con.setLastName(sc.nextLine());
+                        break;
+
+                    case "3" :
+                        System.out.println("Enter new address");
+                        con.setAddress(sc.nextLine());
+                        break;
+
+                    case "4" :
+                        System.out.println("Enter new city");
+                        con.setCity(sc.nextLine());
+                        break;
+
+                    case "5" :
+                        System.out.println("Enter new state");
+                        con.setState(sc.nextLine());
+                        break;
+
+                    case "6" :
+                        System.out.println("Enter new zip");
+                        con.setZip(sc.nextLine());
+                        break;
+
+                    default:
+                        System.out.println("Please make a valid choice");
+
+                }
+
+                }else{
+                System.out.println(name+"not found in the address-book");
+
+
+
+            }
+        }
+    }
+
 
 }
