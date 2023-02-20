f495681f8cda234311714a8f2b591bb6489c2fbc
Your Name <nanawarechaitali4196@gmail.com>
2023-02-20 14:03:02 +0530

    Added MultipleAddressBooks

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -11,23 +11,63 @@ public class AddressBookMain {
         addressBookMethods.newContact();
         addressBookMethods.newContact();
 
-        System.out.println(addressBookMethods.list);
+        MultipleAddressBook obj = new MultipleAddressBook();
 
-        System.out.println("If you want to edit any contact press 1");
-        System.out.println("If you want to delete any contact press 2");
-        int check= scanner.nextInt();
-        if(check==1){
-            addressBookMethods.editContact();
+        while (true) {
+            System.out.println("Enter \n 1. Add new AddressBook\n 2.Add Contact in AddressBook\n" +
+                    "3.edit the contact in AddressBook \n 4.delete the contact in AddressBook \n 5.delete the AddressBook \n"
+                    + "6.Print the AddressBook\n 7. Print contact in AddressBook\n 0.To exit");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    obj.addAddressBook();
+                    break;
+                case 2:
+                    obj.addContact();
+                    break;
+                case 3:
+                    obj.editContactInBook();
+                    break;
+                case 4:
+                    obj.deleteAddressBok();
+                    ;
+                    break;
+                case 5:
+                    obj.deleteAddressBok();
+                    break;
+                case 6:
+                    obj.printContactsInBook();
+                    break;
+                case 7:
+                    obj.PrintBook();
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter the wrong Input");
 
-        }else if(check==2){
-            addressBookMethods.deleteContact();
 
-        }else{
-            System.out.println("Thanks for using this address-book");
+            }
+
+            System.out.println(addressBookMethods.list);
+
+            System.out.println("If you want to edit any contact press 1");
+            System.out.println("If you want to delete any contact press 2");
+            int check = scanner.nextInt();
+            if (check == 1) {
+                addressBookMethods.editContact();
+
+            } else if (check == 2) {
+                addressBookMethods.deleteContact();
+
+            } else {
+                System.out.println("Thanks for using this address-book");
+            }
+            System.out.println(addressBookMethods.list);
+
+
         }
-        System.out.println(addressBookMethods.list);
-
-
-
     }
 }
---
+++ src/com/bridgelabz/MultipleAddressBook.java
@@ -0,0 +1,69 @@
+package com.bridgelabz;
+import java.util.*;
+public class MultipleAddressBook {
+    Map<String,AddressBookMethods> addressBookMap = new HashMap<>();
+    List<Contact> contacts = new ArrayList<>();
+    public void addAddressBook(){
+        System.out.println("Enter Name of new Address Book:");
+        Scanner scanner = new Scanner(System.in);
+        String bookName = scanner.next();
+        if(addressBookMap.containsKey(bookName)){
+            System.out.println("Address book with this name exists,enter new name");
+            addAddressBook();
+        }else{
+            AddressBookMethods addressBook = new AddressBookMethods();
+            addressBookMap.put(bookName,addressBook);
+            System.out.println("Address Book" + bookName+"Successfully added!");
+        }
+    }
+    public void addContact(){
+        System.out.println("Enter the name of Address book to add the contact");
+        Scanner scanner = new Scanner(System.in);
+        String newContact = scanner.nextLine();
+        AddressBookMethods  addressBook=addressBookMap.get(newContact);
+        if(addressBook==null){
+            System.out.println("No book found");
+        }else{
+            addressBookMap.get(newContact).newContact();
+        }
+    }
+    public void editContactInBook(){
+        System.out.println("Enter the name of addressbook you want to edit");
+        Scanner scanner = new Scanner(System.in);
+        String editBookName =scanner.next();
+        if(addressBookMap.containsKey(editBookName)){
+            addressBookMap.get(editBookName).editContact();
+        }else{
+            System.out.println("AddressBook doesnt exist,please enter correct name");
+            editContactInBook();
+        }
+    }
+    public void deleteAddressBok(){
+        System.out.println("Enter name of addressbook you want to delete");
+        Scanner scanner = new Scanner(System.in);
+        String bookName = scanner.next();
+        if(addressBookMap.containsKey(bookName)){
+            addressBookMap.remove(bookName);
+
+        }else{
+            System.out.println("AddressBook doesnt exist,Please delete the contact in it");
+            deleteAddressBok();
+        }
+    }
+    public void PrintBook(){
+        System.out.println("This are AddressBooks in Program");
+        for(String book:addressBookMap.keySet()){
+            System.out.println(book);
+
+        }
+
+    }
+    public void printContactsInBook(){
+        for(String i : addressBookMap.keySet()){
+            System.out.println(i);
+            System.out.println(addressBookMap.get(i).list);
+
+        }
+        System.out.println(" ");
+    }
+}
